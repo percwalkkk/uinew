4c75cae20bf501fdd27f4ae6e1049a0be25f1e8b
percwalkkk <110597529+percwalkkk@users.noreply.github.com>
2025-08-22 17:23:36 -0400

    Update Sharp Ui.lua

--- Sharp Ui.lua
+++ Sharp Ui.lua
@@ -824,7 +824,7 @@ do
 
 			local Logo = Instance.new("ImageLabel")
 			Logo.Name = "Logo"
-			Logo.Image = "http://www.roblox.com/asset/?id=138458025642388"
+			Logo.Image = "http://www.roblox.com/asset/?id=125130621718335"
 			Logo.ScaleType = Enum.ScaleType.Fit
 			Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 			Logo.BackgroundTransparency = 1
